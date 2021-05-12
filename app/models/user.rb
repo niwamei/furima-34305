c91f735489e1a7633851a18f2801cd7d8a43434f
niwamei <niwamei@outlook.com>
2021-05-12 21:08:21 +0900

    パスワードのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validate :password_complexity
+
+  def password_complexity
+  return if password.blank? || password =~ /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/
+
+  errors.add :password, 'Complexity requirement not met. Please use at least 1 letter and 1 number'
+  end
+
   validates :nickname, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
