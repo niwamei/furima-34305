06b2737de3e8bf095778026bf2a8f8b339e0da53
niwamei <niwamei@outlook.com>
2021-05-25 20:45:44 +0900

    コントローラーの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :price, :delivery_fee_id, :delivery_area_id, :delivery_date_id, :image).merge(user_id: current_user.id)
+  end
 end
