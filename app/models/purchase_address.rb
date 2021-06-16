f17e31f288dcb10c22299c6e2d1a5258f795eb42
niwamei <niwamei@outlook.com>
2021-06-16 21:28:07 +0900

    バリデーションの設定

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -6,10 +6,10 @@ class PurchaseAddress
     validates :user_id, :item_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   end
-  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
   def save
     purchase_history = PurchaseHistory.create(user_id: user_id, item_id: item_id)
-    delivery_address = DeliveryAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, purchase_history_id: purchase_history.id)
+    DeliveryAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, purchase_history_id: purchase_history.id)
   end
 end
