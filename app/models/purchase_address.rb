a141110bd23dcee8a36c9e45e47c7c9107959eba
niwamei <niwamei@outlook.com>
2021-06-25 10:50:34 +0900

    バリデーションの記述

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,12 +1,23 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :purchase_history_id
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :purchase_history_id, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :user_id, :item_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :address
+    validates :user_id
+    validates :item_id
+    validates :token
+    with_options format: { with: /\A[0-9]{3}-[0-9]{4}\z/ } do
+      validates :postal_code
+    end
+    with_options numericality: { other_than: 1 } do
+      validates :prefecture_id
+    end
+    with_options length: { maximum: 11 } do
+      validates :phone_number
+    end
   end
-  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
   def save
     purchase_history = PurchaseHistory.create(user_id: user_id, item_id: item_id)
