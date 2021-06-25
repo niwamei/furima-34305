ca8ddcdc198cd2948a27c9070a5b4914aaffded6
niwamei <niwamei@outlook.com>
2021-06-25 10:52:18 +0900

    card決済の実装

--- app/controllers/purchase_histories_controller.rb
+++ app/controllers/purchase_histories_controller.rb
@@ -1,6 +1,16 @@
 class PurchaseHistoriesController < ApplicationController
+  require 'payjp'
+
   def index
     @item = Item.find(params[:item_id])
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+    if current_user.id == @item.user_id 
+      redirect_to root_path
+    elsif user_signed_in? && @item.purchase_history.present?
+      redirect_to root_path
+    end
     @purchase_address = PurchaseAddress.new
   end
 
@@ -8,6 +18,7 @@ class PurchaseHistoriesController < ApplicationController
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
       redirect_to root_path
     else
@@ -18,6 +29,15 @@ class PurchaseHistoriesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
   end
 end
